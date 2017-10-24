9f66643761586dd065e7091695e326bc008f61bc
Javier Esteban Pérez Rivas <alu0100946499@ull.edu.es>
2017-10-24 19:42:26 +0100

    Estructura creada con Bundle

---
+++ spec/alimento_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Alimento do
+  it "has a version number" do
+    expect(Alimento::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
